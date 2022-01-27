0c9f871559712e1f00927cf10729ecceb0dcd62a
IvanHrytsel <ivan.hrytsel.jazzteam@gmail.com>
2022-01-27 16:11:29 +0300

    Commit3

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
 public class Main {
+    private static String state;
+
     public static void main(String[] args) {
         doStuff();
+        if (isStuffDone()) {
+            state = "cool";
+        }
     }
 
     private static String doStuff() {
